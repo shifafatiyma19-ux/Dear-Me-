3c93dacf00652d8084e72963d118dcf442befd91
shifafatiyma19-ux <shifafatiyma19@gmail.com>
2026-08-21 11:01:39 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,98 @@
+const CACHE_NAME = "dearme-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+
+self.addEventListener(
+    "install",
+    function(event) {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+                .then(
+                    function(cache) {
+
+                        return cache.addAll(
+                            FILES_TO_CACHE
+                        );
+
+                    }
+                )
+
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    function(event) {
+
+        event.waitUntil(
+
+            caches.keys()
+                .then(
+                    function(cacheNames) {
+
+                        return Promise.all(
+
+                            cacheNames.map(
+                                function(cacheName) {
+
+                                    if (
+                                        cacheName !==
+                                        CACHE_NAME
+                                    ) {
+
+                                        return caches.delete(
+                                            cacheName
+                                        );
+
+                                    }
+
+                                }
+                            )
+
+                        );
+
+                    }
+                )
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    function(event) {
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            )
+            .then(
+                function(response) {
+
+                    return response ||
+                           fetch(event.request);
+
+                }
+            )
+
+        );
+
+    }
+);
